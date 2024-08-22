0fb6085c1dbc75498ad0218ab6a55ec2189b8640
Gbolahan Akande <geakande@gmail.com>
2024-08-23 00:07:21 +0100

    Added a UI for my supply chain stacks clarity project

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Connect } from '@stacks/connect-react';
+import { StacksTestnet } from '@stacks/network';
+import './App.css';
+
+function App() {
+  const appDetails = {
+    name: 'Supply Chain Management',
+    icon: window.location.origin + '/logo512.png',
+  };
+
+  const network = new StacksTestnet();
+
+  return (
+    <Connect
+      authOptions={{
+        appDetails,
+        redirectTo: '/',
+        onFinish: () => {
+          window.location.reload();
+        },
+        userSession: userSession,
+      }}
+    >
+      <div className="App">
+        <h1>Supply Chain Management</h1>
+      </div>
+    </Connect>
+  );
+}
+
+export default App;
