67d3bcb837e5ba50dc23046af302612955954b87
Gbolahan Akande <geakande@gmail.com>
2024-08-24 13:06:14 +0100

    Corrected and upgraded the overall UI stylying to perfection code for stacks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Connect } from '@stacks/connect-react';
+import { UserSession, AppConfig } from '@stacks/connect';
 import { StacksTestnet } from '@stacks/network';
 import AddProduct from './components/AddProduct/AddProduct';
 import UpdateProductStage from './components/UpdateProductStage/UpdateProductStage';
 import UpdateProductAuthenticity from './components/UpdateProductAuthenticity/UpdateProductAuthenticity';
 import GetProductInfo from './components/GetProductInfo/GetProductInfo';
 import AddUserToRole from './components/AddUserToRole/AddUserToRole';
-import './App.css';
+import './styles/common.css';
 
 function App() {
+  const appConfig = new AppConfig(['store_write', 'publish_data']);
+  const userSession = new UserSession({ appConfig });
+
   const appDetails = {
     name: 'Supply Chain Management',
     icon: window.location.origin + '/logo512.png',
